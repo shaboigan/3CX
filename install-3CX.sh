28ba02476b811baef869e6546a26b1d5cd66ac88
shaboigan <geof.grouch.88@gmail.com>
2019-05-16 14:33:18 +1000

    Initial commit

---
+++ install-3CX.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+Colour='\033[1;31m'
+less='\033[0m'
+
+echo -e "${Colour}By using this script, you'll remove the default pi account, update the system, and install the 3CX SBC (Session Border Controller).\nUse CTRL+C to cancel the script\n\n${less}"
+
+echo -e "${Colour}\nRemoving the default pi account and the associated home directory\n${less}"
+sudo deluser --remove-home pi
+
+echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
+
+echo -e "${Colour}\n\nThe 3CX SBC (Session Border Controller) is downloading now.\n\n${less}"
+wget http://downloads.3cx.com/downloads/sbc/3cxsbc.zip -O 3cxsbc.zip
+
+echo -e "${Colour}\n\nTthe 3CX SBC (Session Border Controller) will be installed now.\n\n${less}"
+sudo bash 3cxsbc.zip
