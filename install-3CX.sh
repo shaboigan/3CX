60f0dc248fbf7aa181506aa3729d56612a26b79d
shaboigan <geof.grouch.88@gmail.com>
2019-05-17 09:20:38 +1000

    Update install-3CX.sh

--- install-3CX.sh
+++ install-3CX.sh
@@ -6,10 +6,16 @@ less='\033[0m'
 echo -e "${Colour}By using this script, you'll remove the default pi account, update the system, and install the 3CX SBC (Session Border Controller).\nUse CTRL+C to cancel the script\n\n${less}"
 
 echo -e "${Colour}\nRemoving the default pi account and the associated home directory\n${less}"
-sudo deluser --remove-home pi
+sudo deluser pi
 
 echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
+
+echo -e "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
+wget wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb -O teamviewer.deb
+
+echo -e "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
+sudo dpkg -i teamviewer.deb; sudo apt-get install -f -y
 
 echo -e "${Colour}\n\nThe 3CX SBC (Session Border Controller) is downloading now.\n\n${less}"
 wget http://downloads.3cx.com/downloads/sbc/3cxsbc.zip -O 3cxsbc.zip
