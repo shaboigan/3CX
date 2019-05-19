e21471316420dfdd70dbaa547317475a963fcfec
shaboigan <geof.grouch.88@gmail.com>
2019-05-20 09:26:33 +1000

    Update new_account.sh

--- new_account.sh
+++ new_account.sh
@@ -1,18 +1,21 @@
 #! /bin/bash
 
-sudo touch file && sudo rm file
-echo "By using this script, you'll create a new user account. Use CTRL+C to cancel the script"
+Colour='\033[1;31m'
+less='\033[0m'
 
-echo "Set the Account Username"
+sudo touch file && sudo rm file
+echo "${Colour}By using this script, you'll create a new user account. Use CTRL+C to cancel the script${less}"
+
+echo "${Colour}Set the Account Username${less}"
 read -p "Please enter the desired Username: " username
 sudo useradd -m -s /bin/bash $username && sudo usermod -aG adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev $username
 
-echo "Set the Account Password"
+echo "${Colour}Set the Account Password${less}"
 sudo passwd $username
 
-echo "Removing pi account from autologin ready for removal"
+echo "${Colour}Removing pi account from autologin ready for removal${less}"
 sudo sed -i "s/autologin-user=pi/autologin-user=$username/g" /etc/lightdm/lightdm.conf && sudo sed -i "s/--autologin pi/--autologin $username/g" /etc/systemd/system/autologin@.service
 
-echo "Script will now reboot."
-echo "Restart the putty session and login with the newly created account"
+echo "${Colour}Script will now reboot.${less}"
+echo "${Colour}Restart the putty session and login with the newly created account${less}"
 sudo reboot
