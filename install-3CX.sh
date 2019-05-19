a97fde0aad76851faa4a94fb922473f5e429a3ee
shaboigan <geof.grouch.88@gmail.com>
2019-05-20 09:51:16 +1000

    tweaks

--- install-3CX.sh
+++ install-3CX.sh
@@ -3,26 +3,26 @@
 Colour='\033[1;31m'
 less='\033[0m'
 
-echo "${Colour}By using this script, you'll remove the default pi account, update the system, install Teamviewer and install the 3CX SBC (Session Border Controller).\nUse CTRL+C to cancel the script\n\n${less}"
+echo -e "${Colour}By using this script, you'll remove the default pi account, update the system, install Teamviewer and install the 3CX SBC (Session Border Controller).\nUse CTRL+C to cancel the script\n\n${less}"
 
-echo "${Colour}\nRemoving the default pi account and the associated home directory\n${less}"
+echo -e "${Colour}\nRemoving the default pi account and the associated home directory\n${less}"
 sudo deluser pi
 
-echo "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
+echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 
-echo "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
+echo -e "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
 wget wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb -O teamviewer.deb
 
-echo "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
+echo -e "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
 sudo dpkg -i teamviewer.deb; sudo apt-get install -f -y
 
-echo "${Colour}\nSet the Teamviewer password\n${less}"
+echo -e "${Colour}\nSet the Teamviewer password\n${less}"
 read -p "Please enter the desired password for Teamviewer (12 Characters or less): " password
 sudo teamviewer passwd $password
 
-echo "${Colour}\n\nThe 3CX SBC (Session Border Controller) is downloading now.\n\n${less}"
+echo -e "${Colour}\n\nThe 3CX SBC (Session Border Controller) is downloading now.\n\n${less}"
 wget http://downloads.3cx.com/downloads/sbc/3cxsbc.zip -O 3cxsbc.zip
 
-echo "${Colour}\n\nTthe 3CX SBC (Session Border Controller) will be installed now.\n\n${less}"
+echo -e "${Colour}\n\nTthe 3CX SBC (Session Border Controller) will be installed now.\n\n${less}"
 sudo bash 3cxsbc.zip
--- new_account.sh
+++ new_account.sh
@@ -4,18 +4,23 @@ Colour='\033[1;31m'
 less='\033[0m'
 
 sudo touch file && sudo rm file
-echo "${Colour}By using this script, you'll create a new user account. Use CTRL+C to cancel the script${less}"
+echo -e "${Colour}By using this script, you'll create a new user account. Use CTRL+C to cancel the script${less}"
 
-echo "${Colour}Set the Account Username${less}"
-read -p "Please enter the desired Username: " username
+echo -e "${Colour}Set the Account Username${less}"
+read -p "Please enter the desired Username, or press enter to use it3support" username
+
+if [[ -z "$username" ]]; then
+	username='it3support'
+fi
+
 sudo useradd -m -s /bin/bash $username && sudo usermod -aG adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev $username
 
-echo "${Colour}Set the Account Password${less}"
+echo -e "${Colour}Set the Account Password${less}"
 sudo passwd $username
 
-echo "${Colour}Removing pi account from autologin ready for removal${less}"
+echo -e "${Colour}Removing pi account from autologin ready for removal${less}"
 sudo sed -i "s/autologin-user=pi/autologin-user=$username/g" /etc/lightdm/lightdm.conf && sudo sed -i "s/--autologin pi/--autologin $username/g" /etc/systemd/system/autologin@.service
 
-echo "${Colour}Script will now reboot.${less}"
-echo "${Colour}Restart the putty session and login with the newly created account${less}"
+echo -e "${Colour}Script will now reboot.${less}"
+echo -e "${Colour}Restart the putty session and login with the newly created account${less}"
 sudo reboot
